3ee38b36042a4ce9b07c7f6d99d9e696d42624e0
luis gomez <luisgomezojeda110502@gmail.com>
2022-06-18 23:53:42 -0300

    agregando la función de comparacion de una fecha con el año actual

--- assets/js/calculate-years.js
+++ assets/js/calculate-years.js
@@ -1,13 +1,29 @@
-let years_input = document.querySelector(".years-input");
+const years_input = document.querySelector(".years-input");
+const years_result = document.querySelector(".years-result");
 
-const calculateYears = fecha = undefined => {
+
+let fecha;
+const calculateYears = newDate => {
+ fecha = new Date(newDate);
  let todayLessDate = new Date().getTime() - fecha.getTime();
  let yearsInMilisecons = 1000 * 60 * 60 * 24 * 365;
  let years = Math.floor(todayLessDate / yearsInMilisecons);
 
  return (Math.sign(years) === -1)
- ? console.info(`Faltan ${Math.abs(years)} años para el año ${fecha.getFullYear()}`)
+ ? years_result.innerHTML = `Faltan ${Math.abs(years)} años para el año ${fecha.getFullYear()}`
  : (Math.sign(years) === 1)
- ? console.info(`Han pasado ${years} años desde el año ${fecha.getFullYear()}`)
- :console.info(`No hay diferencia, estamos en el año actual`);
+ ? years_result.innerHTML = `Han pasado ${years} años desde el año ${fecha.getFullYear()}`
+ : years_result.innerHTML = `No hay diferencia, estamos en el año actual`;
 }
+
+const date = (fecha) =>{
+ let validacion = fecha.split("").every(char => Number(char) >= 0 || char === "-" );
+ return (fecha.length = 10 && validacion);
+};
+
+
+const calculate_y = () =>{
+ calculateYears(years_input.value.split("-").join());
+}
+
+
